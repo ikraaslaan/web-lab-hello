3d64128b6e3c5e3b59155e768828f3ba2dd1c942
ikraaslaan <ikraaslaan@gmail.com>
2026-02-24 21:25:51 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama </h1>
+      <h2>LAB -1</h2>
+      <p>Ad Soyad: İkranur Aslan</p>
+      <p>Ogrenci No: 235541084</p>
+    </div>
+  )
+}
+export default App
